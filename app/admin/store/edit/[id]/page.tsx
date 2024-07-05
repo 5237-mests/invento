45ab095f268ba2de9a5ed83bdcc6366259b54194
5237-mests <msfnmulgeta@gmail.com>
2024-07-05 03:53:23 +0300

    Type error fixed.

--- app/admin/store/edit/[id]/page.tsx
+++ app/admin/store/edit/[id]/page.tsx
@@ -138,7 +138,13 @@ export default function Page({ params }: { params: { id: string } }) {
                 value={
                   fieldUpdated ? updatedStore.description : store.description
                 }
-                onChange={handleChange}
+                onChange={(e) => {
+                  setFieldUpdated(true);
+                  setUpdatedStore({
+                    ...updatedStore,
+                    description: e.target.value,
+                  });
+                }}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               />
             </div>
